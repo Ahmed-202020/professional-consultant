cad4d16b4321f600fa0e83daddc9c6bc3ca2dfb0
Ahmed-202020 <ak812554@gmail.com>
2022-03-09 16:53:45 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+$(function(){
+    $(".en").on("click" , function(){
+        $(".lang .text-lang").text($(this).text()) ; 
+        $("html").attr("dir" , "ltr") ;
+    }) ; 
+    $(".ar").on("click" , function(){
+        $(".lang .text-lang").text($(this).text()) ; 
+        $("html").attr("dir" , "rtl") ;
+    }) ; 
+   
+    $("#notifications").modal("show") ; 
+    $("#notifications .btn").on("click" , function(){
+        $("#notifications").modal("hide") ; 
+    });
+});
