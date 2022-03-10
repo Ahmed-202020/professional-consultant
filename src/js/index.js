95a86dd507ee83358fe7bfc700e56cbe796ed0f5
Ahmed-202020 <ak812554@gmail.com>
2022-03-10 16:55:07 +0200

    the last

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,4 @@ $(function(){
         $(".lang .text-lang").text($(this).text()) ; 
         $("html").attr("dir" , "rtl") ;
     }) ; 
-   
-    $("#notifications").modal("show") ; 
-    $("#notifications .btn").on("click" , function(){
-        $("#notifications").modal("hide") ; 
-    });
 });
