48cbbaee0a80bca4fce74df9722055b26a954e3b
Ahmed-202020 <ak812554@gmail.com>
2022-03-14 17:01:55 +0200

    the last

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,23 +1,68 @@
-$(function(){
-    $('.owl-carousel').owlCarousel({
-        // loop: true,
-        rtl:true,
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        autoHeight : true,
+function consultant_slider(){
+    var owl = $("#consultant-slider");
+    owl.owlCarousel({
+        loop: true ,
         margin: 15,
-        nav: true,
-        
+        navigation: true,
+        items: 5,
+        smartSpeed: 1000,
+        dots: true,
+        autoplay: true,
+        center: true,
+        autoplayTimeout: 2000,
+        dotsEach: true,
         responsive: {
             0: {
-                items: 1 , 
+                items: 1
             },
-            600: {
-                items: 2 , 
+            480: {
+                items: 1
             },
-            1000: {
-                items: 6 , 
+            767: {
+                items: 3
             },
+            992: {
+                items: 5
+            },
+            1920: {
+                items: 5
+            }
         }
-    }) ;
-})
+    });
+}
+consultant_slider();
+
+
+function clients_preview(){
+    var owl = $("#preview-slider");
+    owl.owlCarousel({
+        loop: true ,
+        margin: 75 ,
+        navigation: true,
+        items: 3 ,
+        smartSpeed: 1000,
+        dots: true,
+        autoplay: true,
+        center: true,
+        autoplayTimeout: 2000,
+        dotsEach: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            767: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1920: {
+                items: 3
+            }
+        }
+    });
+}
+clients_preview();
