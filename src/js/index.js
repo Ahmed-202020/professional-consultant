1029c1647f31e263332d22d549dd74fc9b07e184
Ahmed-202020 <ak812554@gmail.com>
2022-03-15 17:01:51 +0200

    the last

--- src/js/index.js
+++ src/js/index.js
@@ -7,4 +7,16 @@ $(function(){
         $(".lang .text-lang").text($(this).text()) ; 
         $("html").attr("dir" , "rtl") ;
     }) ; 
+    $(".close-register").on("click" , function(){
+        $(".modal").modal("hide") ; 
+    })
+    $(".eye").on("mousedown" , function(){
+        $(".pass").attr("type" , "text").css("padding-right" , "25px") ; 
+    })
+    $(".eye").on("mouseup" , function(){
+        $(".pass").attr("type" , "password").css("padding-right" , "25px") ; 
+    })
+    $('label[for = photo] input[type=file]').change(function(e){
+        $(".ph").val($(this).val().replace(/.*(\/|\\)/, ''));
+    })
 });
