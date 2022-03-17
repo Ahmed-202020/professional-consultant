0d8a2cd9e8fb78bae21d07bbe406a7e3efae7dc9
Ahmed-202020 <ak812554@gmail.com>
2022-03-17 16:59:41 +0200

    the last

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,30 @@ $(function(){
     $(".eye").on("mouseup" , function(){
         $(".pass").attr("type" , "password").css("padding-right" , "25px") ; 
     })
+    $(".eye-2").on("mousedown" , function(){
+        $(".pass-2").attr("type" , "text").css("padding-right" , "25px") ; 
+        $(".pass").attr("type" , "password").css("padding-right" , "25px") ; 
+    })
+    $(".eye-2").on("mouseup" , function(){
+        $(".pass-2").attr("type" , "password").css("padding-right" , "25px") ; 
+    })
     $('label[for = photo] input[type=file]').change(function(e){
         $(".ph").val($(this).val().replace(/.*(\/|\\)/, ''));
+    }) ; 
+
+    $(".modal-create .form-group").on("submit" , function(e){
+        e.preventDefault();
+        $(".modal-create").hide() ; 
+        $("#otp-phone").modal("show") ; 
     })
+    $(".modal-forget .form-group").on("submit" , function(e){
+        e.preventDefault();
+        $(".modal-forget").hide() ; 
+        $("#otp-pass").modal("show") ; 
+    });
+
+    $("#notifications").modal("show") ; 
+    $("#notifications .btn").on("click" , function(){
+        $("#notifications").modal("hide") ; 
+    });
 });
