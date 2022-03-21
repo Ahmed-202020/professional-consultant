b10c5d0226cdc9b18a28551f594e07465edc09e9
Ahmed-202020 <ak812554@gmail.com>
2022-03-21 23:55:44 +0200

    the last

--- src/js/index.js
+++ src/js/index.js
@@ -59,6 +59,60 @@ $(function(){
         $(".modal-forget").hide() ; 
         $("#otp-pass").modal("show") ; 
     });
+    $(".submit-porpose .form-group").on("submit" , function(e){
+        e.preventDefault();
+        $("#submit-porpose").hide() ; 
+        $("#successful-submit").modal("show") ; 
+    });
+    $(".write-consult .form-group").on("submit" , function(e){
+        e.preventDefault();
+        $("#successful-submit").modal("show") ; 
+    });
+    $(".data-bank .form-group").on("submit" , function(e){
+        e.preventDefault();
+        $("#bank-info").modal("show") ; 
+    });
+
+    let selector = document.querySelector(".submit-porpose .custom-select") ; 
+    let selectField = document.querySelector(".submit-porpose .custom-select select.form-control")
+    let arrow = document.querySelector(".submit-porpose .custom-select .custom-arrow img") ; 
+    selector.addEventListener("mousedown" , function(e){
+        e.preventDefault() ; 
+        const select = selector.children[0] ; 
+        const dropdown = document.createElement("ul") ; 
+        dropdown.className = "selector-option" ; 
+        [...select.children].forEach(option =>{
+            const dropdownOption = document.createElement("li") ; 
+            dropdownOption.textContent = option.textContent ; 
+
+            dropdownOption.addEventListener("mousedown" ,  function(e){
+                e.stopPropagation() ; 
+                select.value = option.value ; 
+                selector.value = option.value ; 
+                select.dispatchEvent(new Event("change")) ; 
+                selector.dispatchEvent(new Event("change")) ; 
+                arrow.setAttribute("src" , "./assets/icons/Icon ionic-md-arrow-dropdown.svg")
+                dropdown.remove() ; 
+            })
+            dropdown.appendChild( dropdownOption) ;
+        })
+        if( !(selector.appendChild(dropdown))){
+            $(selectField).on("mousedown" , function(){
+                selector.appendChild(dropdown) ; 
+            })
+        }else{
+            $(selectField).on("mousedown" , function(){
+                dropdown.remove() ; 
+            })
+        }
+        arrow.setAttribute("src" , "./assets/icons/Icon ionic-md-arrow-dropdown-1.svg");
+        document.addEventListener("click" , function(e){
+            if(!(selector.contains(e.target))){
+                dropdown.remove() ; 
+                arrow.setAttribute("src" , "./assets/icons/Icon ionic-md-arrow-dropdown.svg")
+            }
+        })
+    })
 
     $("#notifications").modal("show") ; 
     $("#notifications .btn").on("click" , function(){
